e4502bcf3194a99eb5df3c13b2ebbab8dbe730a2
visaltyagi <107295081+visaltyagi@users.noreply.github.com>
2024-03-28 11:23:07 +0530

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-17-jre
+sudo apt-get install docker.io -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y 
